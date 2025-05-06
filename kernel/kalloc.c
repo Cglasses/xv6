156d0e7ddd08c0575d7116d69093c958ce51050f
JRJ <1243735236@qq.com>
2025-05-06 22:37:43 +0800

    lab2

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+
+uint64
+kfreeNum(void)
+{
+  uint64 num=0;
+  struct run *r=kmem.freelist;
+  acquire(&kmem.lock);
+  for(;r;r=r->next)
+  {
+    num += PGSIZE;
+  }
+  release(&kmem.lock);
+  return num;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -277,6 +277,8 @@ fork(void)
 
   np->parent = p;
 
+  np->traceMask = p->traceMask;
+
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
@@ -692,4 +694,18 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+uint64 getProcNum(void)
+{
+  struct proc *p;
+  uint64 num=0;
+  for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state != UNUSED) {
+      num ++;
+    }
+    release(&p->lock);
+  }
+  return num;
 }
